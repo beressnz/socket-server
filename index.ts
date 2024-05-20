8fed3ee85a040693086f1a7754d7e0e5e2d30b6c
BERENICE SANCHEZ SANCHEZ <bsanchezsa@guanajuato.gob.mx>
2024-05-20 14:25:39 -0600

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,23 @@
+
+import Server from "./classes/server";
+import { router } from "./routes/router";
+
+import express from "express";
+import cors from 'cors'
+
+const server = new Server();
+
+//paser conm express
+server.app.use(express.urlencoded({extended:true}));
+server.app.use(express.json());
+
+//cors
+server.app.use( cors({ origin: true, credentials: true}) );
+
+// rutas de servicio
+server.app.use('/', router);
+
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+})
